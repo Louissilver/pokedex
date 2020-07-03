084884379ae1570ecb89d3ce3e2936a5cbfb564a
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2020-07-03 00:04:01 -0300

    Melhorias no funcionamento, inserida popup com skills, mudanças no visual

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}/`;
+const baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
+const ul = document.querySelector('[data-js="pokedex"]');
 
 const generatePokemonPromises = () => Array(150).fill().map((_, index) => 
     fetch(getPokemonUrl(index+1))
@@ -15,7 +17,7 @@ const generateHTML = pokemons => pokemons.reduce((accumulator, { name, id, types
     //https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png
 
     accumulator += `
-        <li class="card ${elementTypes[0]}">
+        <li class="card ${elementTypes[0]}" onclick="selectPokemon(${id})">
         <img class="card-image" alt="${name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png" />
             <h2 class="card-title">${id}. ${name}</h2>
             <p class="card-subtitle">${elementTypes.join(' | ')}</p>
@@ -26,8 +28,7 @@ const generateHTML = pokemons => pokemons.reduce((accumulator, { name, id, types
 
 //Função que insere os pokemón na página
 const insertPokemonIntoPage = pokemons => {
-    const ul = document.querySelector('[data-js="pokedex"]');
-    ul.innerHTML = pokemons;
+ul.innerHTML = pokemons;
 }
 
 const pokemonPromises = generatePokemonPromises();
@@ -35,3 +36,39 @@ const pokemonPromises = generatePokemonPromises();
 Promise.all(pokemonPromises)
     .then(generateHTML)
     .then(insertPokemonIntoPage);
+
+const selectPokemon = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
+    const res = await fetch(url);
+    const poke = await res.json();
+    displayPopup(poke);
+}
+
+const displayPopup = (poke) => {
+    console.log(poke);
+    const type = poke.types.map( type => type.type.name).join('/');
+    const move = poke.moves.map( move => '[' + move.move.name + ']').join(' --- ');
+    const htmlString = `
+    <div class="popup">
+        <button id="closeBtn" onclick="closePopup()">Fechar</button>
+        <div class="card">
+            <img class="card-image" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${poke.id}.png"/>
+            <h2 class="card-title">${poke.id}. ${poke.name}</h2>
+            <p>
+            <small>Height: </small>${(poke.height/7).toFixed(2)} m |
+            <small>Weight: </small>${poke.weight/10} kg |
+            <small>Type: </small>${type}
+            <br><br>
+            <small>Skills: </small>${move}
+            </p>
+        </div>
+    </div>
+    `
+    ul.innerHTML = htmlString + ul.innerHTML;
+    console.log(htmlString);
+}
+
+const closePopup = () => {
+    const popup = document.querySelector('.popup');
+    popup.parentElement.removeChild(popup);
+}
